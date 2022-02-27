73bce1f045b4df2822872750d2110cef60ec9ad2
ayaya zhao <486841+ayamomiji@users.noreply.github.com>
2022-02-28 01:39:22 +0800

    feat: add user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users
 
   match "/auth/:provider/callback", to: "sessions#create", via: %i[get post]
 
